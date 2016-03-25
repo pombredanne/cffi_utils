1f652f18cc1771e8a2869b72d57c0e018f7fc1d8
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-25 13:43:57 -0700

    0.34.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ PACKAGE DATA
 ==============================================================================
 '''
 name = 'cffi_utils'
-version = '0.34.1'   # oldver: '0.34'
+version = '0.34.2'   # oldver: '0.34.1'
 url = 'https://github.com/sundarnagarajan/cffi_utils'
 download_url = '%s/tree/%s' % (url, version)
 packages = find_packages()
