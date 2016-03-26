e1521648ecb7052140ed4a57e2d22b68224fe6b3
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-25 17:52:44 -0700

    0.38

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ PACKAGE DATA
 ==============================================================================
 '''
 name = 'cffi_utils'
-version = '0.37'   # oldver: '0.36'
+version = '0.38'   # oldver: '0.37'
 url = 'https://github.com/sundarnagarajan/cffi_utils'
 download_url = '%s/tree/%s' % (url, version)
 packages = find_packages()
@@ -23,10 +23,6 @@ license = (
 )
 provides = ['cffi_utils']
 
-try:
-    long_description = open('README.rst').read()
-except:
-    long_description = description
 author = 'Sundar Nagarajan'
 author_email = ''
 maintainer = author
@@ -76,6 +72,19 @@ ADDL_KWARGS = dict(
 '''
 
 
+def get_longdesc(default=''):
+    '''
+    Returns-->str
+    '''
+    files = ['README.rst', 'README.md', 'README.txt', 'README']
+    for f in files:
+        try:
+            return open(f, 'r').read()
+        except:
+            continue
+    return default
+
+
 def get_dirtree(topdir, dirlist=[]):
     '''
     topdir-->str: must be name of a dir under current working dir
@@ -111,6 +120,8 @@ dirlist = locals().get('data_dirs', None)
 if isinstance(dirlist, list):
     package_dir = {name: name}
     package_data = {name: get_dirtree(topdir=name, dirlist=dirlist)}
+
+long_description = get_longdesc(description)
 
 known_keywords = [
     'name', 'version', 'packages', 'description', 'license',
